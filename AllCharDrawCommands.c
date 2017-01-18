103a4c146be425b0e88507b5fd59baec2c8ec7af
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-01-18 18:29:57 +0000

    Whoop whoop!, All commands encoded, That took a literal ages. The code could be optimized by inserting all the command values into an array and iterating through it - but at the moment I don't have the time to do that.

---
+++ AllCharDrawCommands.c
@@ -0,0 +1,432 @@
+void drawA() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(40);
+}
+
+void drawB() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(160);
+}
+
+void drawC() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(160);	
+}
+
+void drawD() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(160);
+}
+
+void drawE() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(160);
+}
+
+void drawF() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(100);
+	safeZeroTurn(180);
+	safeDrawLine(100);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(160);
+}
+
+void drawG() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(30);
+	safeZeroTurn(180);
+	safeDrawLine(30);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(40);
+}
+
+void drawH(){
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(40);	
+}
+
+
+void drawI(){
+	safeDrawLine(100);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(100);
+}
+
+void drawJ(){
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(100);	
+}
+
+void drawK() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(-153.4);
+	safeDrawLine(134.16);
+	safeZeroTurn(-26.56);
+	safeDrawLine(40);	
+}
+
+void drawL() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(160);
+}
+
+void drawM(){
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(40);
+}
+
+void drawN() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(135);
+	safeDrawLine(169.706);
+	safeZeroTurn(-135);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(40);
+}
+
+void drawO() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(135);
+	safeDrawLine(169.706);
+	safeZeroTurn(-135);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(40);
+}
+
+void drawP() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(160);
+}
+
+void drawQ() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(90);
+	safeZeroTurn(45);
+	safeDrawLine(50);
+	safeZeroTurn(180);
+	safeDrawLine(100);
+	safeZeroTurn(180);
+	safeDrawLine(50);
+	safeZeroTurn(-45);
+	safeDrawLine(70);
+}
+
+void drawR(){
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(-153.43);
+	safeDrawLine(134.16);
+	safeZeroTurn(-26.56);
+	safeDrawLine(40);
+}
+
+void drawS() {
+	safeDrawLine(160);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(90);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(40);	
+}
+
+void drawT() {
+	safeDrawLine(100);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(100);
+}
+
+void drawU() {
+	safeDrawLine(40);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(-90);
+	safeDrawLine(40);
+}
+
+void drawV() {
+	safeDrawLine(100);
+	safeZeroTurn(-116.565);
+	safeDrawLine(134.16);
+	safeZeroTurn(180);
+	safeDrawLine(134.16);
+	safeZeroTurn(-126.870);
+	safeDrawLine(134.16);
+	safeZeroTurn(180);
+	safeDrawLine(134.16);
+	safeZeroTurn(-116.565);
+	safeDrawLine(100);
+}
+
+void drawX() {
+	safeDrawLine(40);
+	safeZeroTurn(-45);
+	safeDrawLine(169.706);
+	safeZeroTurn(180);
+	safeDrawLine(84.853);
+	safeZeroTurn(90);
+	safeDrawLine(84.853);
+	safeZeroTurn(180);
+	safeDrawLine(169.706);
+	safeZeroTurn(-45);
+	safeDrawLine(40);
+}
+
+void drawY() {
+	safeDrawLine(100);
+	safeZeroTurn(-90);
+	safeDrawLine(60);
+	safeZeroTurn(-45);
+	safeDrawLine(84.853);
+	safeZeroTurn(180);
+	safeDrawLine(84.853);
+	safeZeroTurn(-90);
+	safeDrawLine(84.853);
+	safeZeroTurn(180);
+	safeDrawLine(84.853);
+	safeZeroTurn(-45);
+	safeDrawLine(60);
+	safeZeroTurn(-90);
+	safeDrawLine(100);
+}
+
+void drawZ() {
+	safeDrawLine(40);
+	safeZeroTurn(-45);
+	safeDrawLine(169.706);
+	safeZeroTurn(-135);
+	safeDrawLine(120);
+	safeZeroTurn(180);
+	safeDrawLine(120);
+	safeZeroTurn(135);
+	safeDrawLine(169.706);
+	safeZeroTurn(-135);
+	safeDrawLine(160);
+}
