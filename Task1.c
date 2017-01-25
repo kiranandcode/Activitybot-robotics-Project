912117198f3fe210afd63764aa15031efe27033e
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-01-25 20:45:26 +0000

    Added comments to task 1.c

--- Task1.c
+++ Task1.c
@@ -1,11 +1,16 @@
 #include "abdrive.h"
 #include <stdio.h>
 #include "simpletools.h"
-                    // Include simple tools
 
 int mmToTicks(float mm) {
  return mm/3.25; 
 }
+
+//For the project, we decided to use a standard set of
+//units throughout the program to prevent errors later
+//down the line. These units were chosen to be mm and
+//degrees.
+
 
 void moveForward(int length) {
   int ticks = mmToTicks(length);
@@ -17,8 +22,7 @@ void moveForward(int length) {
 
 
 
-int main()                                    // Main function
+int main()   
 {
-
   moveForward(500);
 }
