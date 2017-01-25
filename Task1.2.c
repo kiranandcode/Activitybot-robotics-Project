c9394d18dc8dcf30640535ac74a9140a8d7caa3d
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2017-01-25 20:53:32 +0000

    Neatened Task1.2.C code

--- Task1.2.c
+++ Task1.2.c
@@ -1,22 +1,24 @@
 #include "abdrive.h"
 #include <stdio.h>
 #include "simpletools.h"
-                    // Include simple tools
 
-void zeroRadTurn(float angle){ 
+//All conversion between degrees to ticks is handled
+//by the function, allowing the user to maintain a 
+//constant set of units.
+void zeroRadTurn(float angle){
+  //Calculate the angle to move.
   float result = (((angle/360)*PI*2)*(105.8/2))/3.25;
+  
+  //Move the angle.
   drive_goto((int)result, -1 * (int)result);
+  
   return;
 }
 
 
-
-
-
-
-int main()                                    // Main function
+int main() 
 {
   while(1){
   zeroRadTurn(360);
- }                                            // Display test message
+ }
 }
